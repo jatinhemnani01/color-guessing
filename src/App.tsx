52d9354b2812f71d23ee10338ad14dbd97f6a065
jatinhemnani01 <jatinhemnani01@gmail.com>
2022-11-06 21:28:31 +0530

    added tailwind cdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="flex justify-center items-center flex-col h-full">
+      <div className="w-80 h-60 bg-orange-200	rounded-2xl"></div>
+    </div>
+  )
 }
 
 export default App;
